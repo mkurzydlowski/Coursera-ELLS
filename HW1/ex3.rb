608601d1fcacd3e74d68473231ec16cb0f233dc4
Michal Kurzydlowski <awerin0@gmail.com>
2012-06-29 21:49:31 +0200

    Exercise 3 initial commit.

---
+++ HW1/ex3.rb
@@ -0,0 +1,14 @@
+﻿require "test/unit"
+
+class TestCombineAnagrams < Test::Unit::TestCase
+
+	def test_simple_combine_anagrams
+		assert_equals([["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]],
+			combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']))
+	end
+	
+end
+
+def combine_anagrams(words)
+
+end
