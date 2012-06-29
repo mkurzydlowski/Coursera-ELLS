31fa386f10b074db76b6e28de13bf70ef9793fea
Michal Kurzydlowski <awerin0@gmail.com>
2012-06-29 21:47:18 +0200

    Exercise 2 finished.

--- HW1/ex2.rb
+++ HW1/ex2.rb
@@ -30,7 +30,7 @@ PLAYS = BEATS_PLAY.keys
 
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2
-	raise NoSuchStrategyError unless game.all? { |player| PLAYS.include? play player }
+	raise NoSuchStrategyError, game unless game.all? { |player| PLAYS.include? play player }
 	first_p, second_p = game[0], game[1]
 	
 	(BEATS_PLAY[play second_p].include? play first_p) ? second_p : first_p
@@ -47,13 +47,28 @@ end
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
-class TestRPSGameWinner < Test::Unit::TestCase
+class TestRPSTournamentWinner < Test::Unit::TestCase
 
-	def test_raise_wrong_player_number
-		assert_raise( WrongNumberOfPlayersError ) do
-			rps_game_winner [ [] ]
-		end
-		assert_raise( WrongNumberOfPlayersError ) do
-			rps_game_winner [ [], [], [] ]
-		end
+	def test_simple_tournament_winner
+		assert_equal(["Richard", "R"], rps_tournament_winner(
+			[
+			  [
+				[ ["Armando", "P"], ["Dave", "S"] ],
+				[ ["Richard", "R"], ["Michael", "S"] ],
+			  ],
+			  [
+				[ ["Allen", "S"], ["Omer", "P"] ],
+				[ ["David E.", "R"], ["Richard X.", "P"] ]
+			  ]
+			]))
 	end
+	
+end
+
+def rps_tournament_winner(game)
+	if game[0][0].is_a? Array
+		rps_game_winner([rps_tournament_winner(game[0]), rps_tournament_winner(game[1])])
+	else
+		rps_game_winner(game)
+	end
+end
