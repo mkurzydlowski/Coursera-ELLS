809de62f6770b2790298b8d0b13dd3a7c4200fa0
Michal Kurzydlowski <awerin0@gmail.com>
2012-06-30 11:25:01 +0200

    Ex4a.

--- HW1/ex4.rb
+++ HW1/ex4.rb
@@ -2,8 +2,43 @@
 
 class TestDessert < Test::Unit::TestCase
 
+	name = "ice cream"
+
 	def test_create_dessert
-		assert !Dessert.new("ice cream", "200").nil?
+		assert !Dessert.new(@name, 200).nil?
 	end
 	
+	def test_is_healthy
+		assert Dessert.new(@name, 199).healthy?
+	end
+	
+	def test_is_not_healthy
+		assert !Dessert.new(@name, 200).healthy?
+	end
+	
+	def test_is_delicious
+		assert Dessert.new(@name, 0).delicious?
+	end
+	
+end
+
+class Dessert
+
+	HEALTHY_TRESHOLD = 200
+
+	attr_accessor :name, :calories
+
+	def initialize(name, calories)
+		@name = name
+		@calories = calories
+	end
+	
+	def healthy?
+		@calories < HEALTHY_TRESHOLD
+	end
+	
+	def delicious?
+		true
+	end
+
 end
