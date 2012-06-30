778d2b057d63545913f5e03784e4819ae15d7eb1
Michal Kurzydlowski <awerin0@gmail.com>
2012-06-30 10:52:26 +0200

    Ex 3 finished.

--- HW1/ex3.rb
+++ HW1/ex3.rb
@@ -3,12 +3,18 @@
 class TestCombineAnagrams < Test::Unit::TestCase
 
 	def test_simple_combine_anagrams
-		assert_equals([["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]],
-			combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']))
+		argument = ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']
+		result = [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
+		assert_equal(result.sort, combine_anagrams(argument).sort)
 	end
 	
 end
 
 def combine_anagrams(words)
-
+	groups = Hash.new { default = [] }
+	words.each do |word|
+		key = word.downcase.chars.sort.join
+		groups[key] = groups[key].push(word)
+	end
+	groups.values
 end
