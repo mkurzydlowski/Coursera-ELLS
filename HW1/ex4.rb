03a64beed65bd7325eeaa696132a5e9e870db032
Michal Kurzydlowski <awerin0@gmail.com>
2012-06-30 11:58:07 +0200

    Ex4 finished.

--- HW1/ex4.rb
+++ HW1/ex4.rb
@@ -2,22 +2,38 @@
 
 class TestDessert < Test::Unit::TestCase
 
-	name = "ice cream"
+	NAME = "ice cream"
+	CALORIES = 0
 
-	def test_create_dessert
-		assert !Dessert.new(@name, 200).nil?
+	def test_create
+		assert !Dessert.new(NAME, CALORIES).nil?
 	end
 	
 	def test_is_healthy
-		assert Dessert.new(@name, 199).healthy?
+		assert Dessert.new(NAME, Dessert::HEALTHY_TRESHOLD-1).healthy?
 	end
 	
 	def test_is_not_healthy
-		assert !Dessert.new(@name, 200).healthy?
+		assert !Dessert.new(NAME, Dessert::HEALTHY_TRESHOLD).healthy?
 	end
 	
 	def test_is_delicious
-		assert Dessert.new(@name, 0).delicious?
+		assert Dessert.new(NAME, CALORIES).delicious?
+	end
+	
+end
+
+class TestJellyBean < Test::Unit::TestCase
+
+	NAME = "some jelly"
+	CALORIES = 0
+
+	def test_create
+		assert !JellyBean.new(NAME, CALORIES, "some flavor").nil?
+	end
+	
+	def test_is_not_delicious
+		assert !JellyBean.new(NAME, CALORIES, JellyBean::BAD_FLAVOR).delicious?
 	end
 	
 end
@@ -39,6 +55,23 @@ class Dessert
 	
 	def delicious?
 		true
+	end
+
+end
+
+class JellyBean < Dessert
+
+	BAD_FLAVOR = "black licorice"
+
+	attr_accessor :flavor
+
+	def initialize(name, calories, flavor)
+		super(name, calories)
+		@flavor = flavor
+	end
+	
+	def delicious?
+		flavor == BAD_FLAVOR ? false : super
 	end
 
 end
